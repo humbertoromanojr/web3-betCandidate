14130b1d397f7b320e628caca5e32f4cecf0a18a
Humberto Junior <astronomi@gmail.com>
2025-10-06 19:43:42 -0300

    First commit

---
+++ smart-contract/BetCandidate.sol
@@ -0,0 +1,89 @@
+//SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.26;
+
+struct Bet {
+    uint amount;
+    uint candidate;
+    uint timestamp;
+    uint claimed;
+}
+
+struct Dispute {
+    string candidate1;
+    string candidate2;
+    string image1;
+    string image2;
+    uint total1;
+    uint total2;
+    uint winner; 
+}
+
+contract BetCandidate {
+
+    Dispute public dispute;
+    mapping(address => Bet) public allBets;
+
+    address owner;
+    uint fee = 1000; // 10% (scale to 4 zeros)
+    uint public netPrize;
+
+    constructor(){
+        owner = msg.sender; 
+        dispute = Dispute({
+            candidate1: "Palmeiras",
+            candidate2: "Flamengo",
+            image1: "https://www.vhv.rs/dpng/d/593-5932129_se-palmeiras-hd-logo-png-escudo-png-palmeiras.png",
+            image2: "https://logoeps.com/wp-content/uploads/2012/11/clube-de-regatas-do-flamengo-logo-vector.png",
+            total1: 0,
+            total2: 0,
+            winner: 0
+        });
+    }
+
+    function bet(uint candidate) external payable {
+        require(candidate == 1 || candidate == 2, "Invalid candidate");
+        require(msg.value > 0, "Invalid bet");
+        require(dispute.winner == 0, "Dispute closed");
+
+        Bet memory newBet;
+        newBet.amount = msg.value;
+        newBet.candidate = candidate;
+        newBet.timestamp = block.timestamp;
+
+        allBets[msg.sender] = newBet;
+
+        // increase total candidate
+        if(candidate == 1)
+            dispute.total1 += msg.value;
+        else 
+            dispute.total2 += msg.value;
+    }
+
+    // validate winner only once time and closed the bet
+    function finish(uint winner) external {
+        require(msg.sender == owner, "Invalid account");
+        require(winner == 1 || winner == 2, "Invalid cancidate");
+        require(dispute.winner == 0, "Dispute closed");
+
+        dispute.winner = winner;
+
+        uint grossPrize = dispute.total1 + dispute.total2;
+        uint commission = (grossPrize * fee) / 1e4;
+        netPrize = grossPrize - commission;
+
+        payable(owner).transfer(commission); 
+    }
+
+    function clain() external {
+        Bet memory userBet = allBets[msg.sender];
+        require(dispute.winner > 0 && dispute.winner == userBet.candidate && userBet.claimed == 0, "Invalid clain");
+
+        uint winnerAmount = dispute.winner == 1 ? dispute.total1 : dispute.total2;
+        uint ratio = (userBet.amount * 1e4) / winnerAmount;
+        uint individualPrize = (netPrize * ratio) / 1e4;
+        allBets[msg.sender].claimed = individualPrize;
+        payable(msg.sender).transfer(individualPrize); 
+    }
+
+}
