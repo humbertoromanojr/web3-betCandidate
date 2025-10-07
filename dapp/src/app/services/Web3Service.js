84e1e196d7de51f9687718604bb9481e15bf6432
Humberto Junior <astronomi@gmail.com>
2025-10-07 16:20:55 -0300

    Finish doLogin function

--- dapp/src/app/services/Web3Service.js
+++ dapp/src/app/services/Web3Service.js
@@ -1,3 +1,14 @@
 import Web3 from "web3";
 
-export async function doLogin() {}
+export async function doLogin() {
+  if (!window.ethereum) throw new Error("Metamask not installed!");
+
+  const web3 = new Web3(window.ethereum);
+  const accounts = await web3.eth.requestAccounts();
+  if (!accounts || !accounts.length)
+    throw new Error("Metamask not connected or authorization!");
+
+  localStorage.setItem("addressWallet", accounts[0]);
+
+  return accounts[0];
+}
