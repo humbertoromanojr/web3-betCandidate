11fa44c15bf47e11eb91cfa4d81294a68382c80c
Humberto Junior <astronomi@gmail.com>
2025-10-07 20:24:21 -0300

    Updated with functions getContract and getDispute

--- dapp/src/app/services/Web3Service.js
+++ dapp/src/app/services/Web3Service.js
@@ -1,4 +1,9 @@
 import Web3 from "web3";
+require("dotenv");
+
+import ABI from "./ABI.json";
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS_BETCANDIDATE;
 
 export async function doLogin() {
   if (!window.ethereum) throw new Error("Metamask not installed!");
@@ -11,4 +16,18 @@ export async function doLogin() {
   localStorage.setItem("addressWallet", accounts[0]);
 
   return accounts[0];
+}
+
+function getContract() {
+  if (!window.ethereum) throw new Error("Metamask not installed!");
+
+  const from = localStorage.getItem("addressWallet");
+  const web3 = new Web3(window.ethereum);
+  const contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from });
+  return contract;
+}
+
+export async function getDispute() {
+  const contract = getContract();
+  return await contract.methods.dispute().call();
 }
